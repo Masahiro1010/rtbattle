de15953970950bccbbc1859a4f2fd12a8183ce50
Masahiro <yasui.masahiro7316@gmail.com>
2025-10-30 01:38:22 +0900

    final3

--- rtbattle/asgi.py
+++ rtbattle/asgi.py
@@ -1,13 +1,15 @@
-# config/asgi.py
+# rtbattle/asgi.py
 import os
 import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from arena import routing as arena_routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rtbattle.settings")
+django.setup()  # ← これが routing を import するより先！
+
+from arena import routing as arena_routing  # ← ここで初めて import する
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -15,4 +17,3 @@ application = ProtocolTypeRouter({
         URLRouter(arena_routing.websocket_urlpatterns)
     ),
 })
-
